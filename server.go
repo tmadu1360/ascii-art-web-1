1a54d0f5fd67d3161373dc9ed1eb50d4666c5d5e
VDAUTREMENT <valentin.dautrement@ynov.com>
2020-12-07 10:25:18 +0100

    Push Final version 0.0.1

--- server.go
+++ server.go
@@ -10,25 +10,13 @@ import (
 
 func startPage(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		http.Error(w, "404 Page not found.", http.StatusNotFound)
-	}
-
-	if r.Method != "GET" {
-		http.Error(w, "Method not supported !", http.StatusNotFound)
-	}
-
-	http.ServeFile(w, r, "start.html")
-}
-
-func postHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/form" {
-		http.Error(w, "404 not found.", http.StatusNotFound)
+		http.ServeFile(w, r, "www/error404.html")
 		return
 	}
 
 	switch r.Method {
 	case "GET":
-		http.ServeFile(w, r, "form.html")
+		http.ServeFile(w, r, "www/")
 	case "POST":
 		// Call ParseForm() to parse the raw query and update r.PostForm and r.Form.
 		if err := r.ParseForm(); err != nil {
@@ -37,7 +25,7 @@ func postHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		fmt.Fprintf(w, "Post from website! r.PostFrom = %v\n", r.PostForm)
 		text := r.FormValue("text")
-		font := r.FormValue("font")
+		font := r.FormValue("style")
 		out := asciiart.AsciiArt(text, font)
 		fmt.Fprintf(w, out)
 	default:
@@ -46,10 +34,17 @@ func postHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", startPage)
-	http.HandleFunc("/form", postHandler)
+	//http.Handle("/", http.FileServer(http.Dir("www/")))
+	fs := http.FileServer(http.Dir("www/css"))
+	http.Handle("/css/", http.StripPrefix("/css/", fs))
+	police := http.FileServer(http.Dir("www/font"))
+	http.Handle("/font/", http.StripPrefix("/font/", police))
+	img := http.FileServer(http.Dir("www/image"))
+	http.Handle("/image/", http.StripPrefix("/image/", img))
 
-	fmt.Printf("Starting server for testing HTTP POST...\n")
+	http.HandleFunc("/", startPage)
+
+	fmt.Printf("Starting server for testing HTTP on port 8080...\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
